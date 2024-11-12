f5fc2c716b7a0800668c7fbf1b01a8f3165f76c0
Kamilq99 <83961352+Kamilq99@users.noreply.github.com>
2024-11-12 15:21:54 +0100

    Adding the part of the register service

--- register_service/register_service.go
+++ register_service/register_service.go
@@ -1,30 +1,15 @@
 package main
 
 import (
+	"log"
 	"net/http"
-
-	"github.com/gin-gonic/gin"
 )
 
-type User struct {
-	ID       uint   `json:"id"`
-	Username string `json:"username"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
-func postRegister(con *gin.Context) {
-
-	var user User
-	if err := con.ShouldBindJSON(&user); err != nil {
-		con.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	con.JSON(http.StatusOK, gin.H{"data": user})
-}
-
 func main() {
-	router := gin.Default()
-	router.POST("/register", postRegister)
-	router.Run(":8081")
+
+	log.Println("Register Service start on localhost:8080")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
